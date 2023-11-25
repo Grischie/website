c2cea3b9993f25f2017f2cf49d5a9f137f9c3adc
Grischie <simgri@hotmail.de>
2023-11-25 13:29:09 +0100

    connection with db to login

--- projects/php_ws2023/00_online_banking_system/login.php
+++ projects/php_ws2023/00_online_banking_system/login.php
@@ -1,7 +1,32 @@
 <?php
 session_start(); 
-if (isset($_POST["name"]) && $_POST["name"] == "Hans" && $_POST["passwort"] == "geheim") { 
-	$_SESSION["name"] = "Hans"; 
+
+//Password für die Datenbank importierens
+include('password.inc.php');	
+
+//Connection zur Dantenbank herstellen
+$con = mysqli_connect("localhost", "root", $ps, "online_banking");
+
+//SQL Statement herstellen
+$sql = "select name, password form User where name = " . $_POST["name"];
+
+//SQL Query senden
+$res = mysqli_query($con, $sql);
+
+//Anzahl der SQL ergebnisse 
+$num = mysqli_num_rows($res);
+
+//Prüfe ob Datensätze vorhanden sind
+if ($num==0) {
+	header("Location: start.php?f=1"); 
+exit;
+} else {
+	//Alle Daten aus der Datenbank holen
+	while ($dsatz = mysqli_fetch_assoc($res));	
+}
+//Prüfen ob die eingegebnen Daten korrekt sind
+if (isset($_POST["name"]) && $_POST["name"] == $dsatz["name"] && $_POST["passwort"] == $dsatz["password"]) {
+	$_SESSION["name"] = $_POST["name"]; 
 	$_SESSION["login"] = "ok";
 	header("Location: willkommen.php");
 	exit;
--- projects/php_ws2023/00_online_banking_system/sql_con.php
+++ projects/php_ws2023/00_online_banking_system/sql_con.php
@@ -1,10 +1,23 @@
 <?php
+//Password für die Datenbank importierens
 include('password.inc.php');
+
+//Connection zur Dantenbank herstellen
 $con = mysqli_connect("localhost", "root", $ps, "online_banking");
-$res = mysqli_query($con, "select * from User");
 
-$dsatz = mysqli_fetch_assoc($res);
+//SQL Statement herstellen
+$sql = "select name, password form User where name = " . $_POST["name"];
 
-echo $dsatz['name'];
+//SQL Query senden
+$res = mysqli_query($con, $sql);
 
+//Anzahl der SQL ergebnisse 
+$num = mysqli_num_rows($res);
+
+//Wenn keine Ergebnisse vorhanden ist 
+if ($num==0) echo "Keine passenden Datensätze gefunden";
+
+
+while ($dsatz = mysqli_fetch_assoc($res));
+echo $dsatz;
 ?>
