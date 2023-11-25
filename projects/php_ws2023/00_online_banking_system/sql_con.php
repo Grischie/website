6e7570c7dac080618531efa0d171a5bec4aa685f
Grischie <simgri@hotmail.de>
2023-11-25 14:01:12 +0100

    testing

--- projects/php_ws2023/00_online_banking_system/sql_con.php
+++ projects/php_ws2023/00_online_banking_system/sql_con.php
@@ -7,15 +7,14 @@ $con = mysqli_connect("localhost", "root", $ps, "online_banking");
 
 $username = "test_user1";
 //SQL Statement herstellen
-//$sql = "select name, password from User";
-//$sql .= " where name = " . $username;
-$sql = 'select * from User where name = "test_user1"' ;
+$sql = "select name, password from User";
+$sql .= ' where name = "'. $username .'"';
+
 //SQL Query senden
 $res = mysqli_query($con, $sql);
 
 //Datensatz holen
 while($dsatz = mysqli_fetch_assoc($res)){
     echo $dsatz["name"];
-    echo $dsatz["mail"];
 }	
 ?>
