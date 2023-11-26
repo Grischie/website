1a38b4a5a824c383c76c14b00da114690d71656a
Grischie <simgri@hotmail.de>
2023-11-26 13:20:23 +0100

    testing

---
+++ projects/php_ws2023/00_online_banking_system/testing.php
@@ -0,0 +1,20 @@
+<?php
+session_start(); 
+
+include('password.inc.php');	
+
+//Connection zur Dantenbank herstellen
+$con = mysqli_connect("localhost", "root", $ps, "online_banking");
+
+
+
+//Neue Kontonummer generieren 
+$sql_max = "select max(kontonummer) from User";
+$res_max = mysqli_query($con, $sql_max);
+$max_kn = mysqli_fetch_assoc($res_max);	
+echo $max_kn;
+$max_kn = intval($max_kn["Kontonummer"]);
+echo $max_kn;
+$new_kn = $max_kn + 1;
+echo $max_kn;
+?>
