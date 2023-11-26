c40904c47ab075164a6ce281db2ccadd6c82463c
Grischie <simgri@hotmail.de>
2023-11-26 12:58:45 +0100

    testing

--- projects/php_ws2023/00_online_banking_system/registration.php
+++ projects/php_ws2023/00_online_banking_system/registration.php
@@ -34,6 +34,7 @@ if (isset($_POST["vorname"]) && $_POST["vorname"] != "" && isset($_POST["name"])
     $sql_max = "select max(kontonummer) from User";
     $res_max = mysqli_query($con, $sql_max);
     $max_kn = mysqli_fetch_assoc($res_max);	
+    $max_kn = $max_kn["Kontonummer"];
     $new_kn = $max_kn + 1;
 
     //Art des Konto definieren
