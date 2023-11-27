66192ca6190704b45a4b3f81d1ce42fbc450a892
Grischie <simgri@hotmail.de>
2023-11-27 09:58:31 +0100

    update

---
+++ projects/php_ws2023/00_online_banking_system/anfrage.php
@@ -0,0 +1,26 @@
+<?php
+session_start(); 
+include('password.inc.php');	
+
+$con = mysqli_connect("localhost", "root", $ps, "online_banking");
+
+if (isset($_POST["kontonummer"]) && $_POST["kontonummer"] != "" && $_POST["betrag"] != "" && $_POST["kondition"] != "") {
+
+	$sql_new = "insert into Kredite (kreditanbieter, nachfragender, betrag, kondition, status) values "
+	. "('" 
+	. $_POST["kontonummer"] . "', '"
+    . $_SESSION["kontonummer"] . "', '"
+	. $_POST["betrag"] . "', '"
+	. $_POST["kondition"] . "', '"
+	. "angefragt"."'"
+	.")";
+
+	mysqli_query($con, $sql_new);
+	mysqli_close($con);
+	header("Location: kredit_ausschreiben.php?f=2"); 
+	exit;
+} else {
+	header("Location: kredit_ausschreiben.php?f=1"); 
+exit;
+} 
+?>
---
+++ projects/php_ws2023/00_online_banking_system/kreditanfragen.php
@@ -0,0 +1,79 @@
+<?php
+	session_start();
+	if (isset($_SESSION["login"]) && $_SESSION["login"] == "ok") {
+        if ($_SESSION["rechte"] != 1){
+			header("Location: kreditanbietender.php");
+			exit;
+		}
+?>  
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Kredit anfragen</title>
+    <style>
+		.fehler { color: red; }
+        .ok { color: green; }
+	</style>
+</head>
+<body>
+    <h1>Kredit anfragen</h1>
+	<?php
+		echo "<h2>Kontonummer: {$_SESSION['kontonummer']}<br />Kontostand: {$_SESSION['status']}€";
+	?>
+    <h2>Kreditanbieter Liste</h2>
+    <?php 
+		include('password.inc.php');	
+
+        $con = mysqli_connect("localhost", "root", $ps, "online_banking");
+        
+        $sql = "select name, vorname, kontonummer from User";
+        $sql .= ' where rechte = "1"';
+
+        $res = mysqli_query($con, $sql);
+        echo "<table border='1'>";
+        echo "<tr> <td>Name</td><td>Vorname</td><td>Kontonummer</td>";
+        while ($dsatz = mysqli_fetch_assoc($res)){
+            echo "<tr>";
+            echo "<td>" . $dsatz["name"] . "</td>";
+            echo "<td>" . $dsatz["vorname"] . "</td>";
+            echo "<td>" . $dsatz["kontonummer"] . "</td>";
+            echo "</tr>";
+            }
+        echo "</table>";
+        mysqli_close($con);
+	?>
+    <h2>Anfragen</h2>
+    <?php 
+		if (isset($_GET["f"]) && $_GET["f"] == 1) {
+			echo "<p class='fehler'>Anfrage nicht erfolgreich</p>";
+		}
+	?>
+    <p>
+    <form action="anfrage.php" method="post" >
+		Kontonummer: <br />
+		<input type="number" name="kontonummer" size="20" /><br /><br />
+		Betrag: <br />
+		<input type="number" name="betrag"size="20" /><br /><br />
+        Kondition: <br />
+		<input type="text" name="kondition"size="60" /><br /><br />
+		<input type="submit" value="Anfragen" />
+	</form>
+    </p>
+    <?php
+        if (isset($_GET["f"]) && $_GET["f"] == 2) {
+			echo "<p class='ok'>Anfrage erfolgreich</p>";
+		}
+    ?>
+	<p><a href="kreditanbietender.php">Zurück</p>
+</body>
+</html>
+<?php
+	} else {
+		$host = htmlspecialchars
+		($_SERVER["HTTP_HOST"]);
+		$uri = rtrim(dirname(htmlspecialchars($_SERVER["PHP_SELF"])),"/\\");
+		$extra = "start_login.php";
+		header("Location:http://$host$uri/$extra");
+	}
+?>
