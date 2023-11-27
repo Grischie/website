9e96ef6ce9c542d07c2f9981d9692003de3ed7f9
Grischie <simgri@hotmail.de>
2023-11-27 14:34:38 +0100

    update

--- projects/php_ws2023/00_online_banking_system/liste_bearbeiten.php
+++ projects/php_ws2023/00_online_banking_system/liste_bearbeiten.php
@@ -3,16 +3,29 @@ session_start();
 include('password.inc.php');	
 
 $con = mysqli_connect("localhost", "root", $ps, "online_banking");
-
+$sql = "select nachfragender, betrag from Kredite";
+$sql .= ' where name = "'. $_POST["auswahl"] .'"';
+$res = mysqli_query($con, $sql);
+$dsatz = mysqli_fetch_assoc($res);
 if (isset($_POST["auswahl"])){
     if($_POST["action"]=="Annehmen"){
+        echo '<form action="send_1.php" method="post" >
+		Konto: <br />
+		<input type="number" name="kontonummer" size="20" value='.$dsatz["nachfragender"] .'/><br /><br />
+		Betrag: <br />
+		<input type="number" name="betrag"size="20" /><br /><br />
+        Zweck: <br />
+		<input type="text" name="kommentar"size="60" value='.$_POST["grund"] .' /><br /><br />
+		<input type="submit" value="überweisen" />
+	    </form>';
+
         $sql_annehmen_1 = "update Kredite set status = 'angenommen" 
         ."' where kontonummer = '"
-        . $_POST["aushwal"]
+        . $_POST["auswahl"]
         ."'";
         $sql_annehmen_2 = "update Kredite set kommentar = '" .$_POST["grund"]
         ."' where kontonummer = '"
-        . $_POST["aushwal"] 
+        . $_POST["auswahl"] 
         ."'";
     }
     
