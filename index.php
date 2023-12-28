7655b71cf724445fd0c4f7d053e7b6bb09a8eb04
Grischie <simgri@hotmail.de>
2023-12-28 15:19:19 +0100

    image update

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 	include_once "header.php";
 ?>
 <div class="wrapper">
-	<div id="hello_line" class="hello_line"></div>
 	<img class="image_simon" src="medien/images/simon.png" alt="Simon Grischek">
+	<div id="hello_line" class="hello_line"></div>
 <?php
 	include_once "footer.php";
 ?>
